eba2fcda2bcd77dce2ed005a925684917c435dcd
Saniya Ashraf <32168225+Saniya-Ashraf@users.noreply.github.com>
2021-11-29 13:03:05 +0500

    contact update

--- forward.php
+++ forward.php
@@ -1,25 +1,19 @@
-Allow: <http-methods>
-<?php
+<?php 
+if(isset($_POST['submit'])){
+    $to = "email@example.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
 
-
-if($_POST["message"]) {
-
-
-mail("carlbuitems@gmail.com", $_POST["subject"],
-$_POST["message"]. 
-	 "From: $_POST["subject"]");
-
-
-}
-
-
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    // You cannot use header and echo together. It's one or the other.
+    }
 ?>
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-</head>
-
-<body>
-</body>
-</html>
